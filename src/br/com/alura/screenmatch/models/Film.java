772bbd2cc7b9e74a45bca19964d671fb085c2d2c
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-03 16:50:52 -0300

    Add constructor and toString method to Film class for improved initialization and representation

--- src/br/com/alura/screenmatch/models/Film.java
+++ src/br/com/alura/screenmatch/models/Film.java
@@ -6,6 +6,15 @@ public class Film extends Title implements Classification {
 
     private  String director;
 
+     public Film(String name, int year, boolean avaliable, int duration, double rate, String director) {
+        super(name, year);
+        setIsAvailableInPlan(avaliable);
+        setSumRatings(rate);
+        setTotalRatings(1);
+        setTimeDuration(duration);
+        this.director = director;
+    }
+
     public String getDirector() {
         return director;
     }
@@ -18,4 +27,11 @@ public class Film extends Title implements Classification {
     public int getClassification() {
         return (int) getAverageRating()/2;
     }
+
+    @Override
+    public String toString() {
+        return "Film: " + getName() + " - " + getReleaseYear();
+    }
+
+
 }
