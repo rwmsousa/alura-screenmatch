8a1d6bed552eb8b877cd773c8017982f7a6ee8a9
Ricardo Sousa <rwmsousa@gmail.com>
2025-01-04 19:02:08 -0300

    Add MainWithLists class to demonstrate usage of Film and Series classes with a watched list

---
+++ src/br/com/alura/screenmatch/principal/MainWithLists.java
@@ -0,0 +1,34 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.models.Film;
+import br.com.alura.screenmatch.models.Series;
+import br.com.alura.screenmatch.models.Title;
+import java.util.ArrayList;
+
+public class MainWithLists {
+
+    public static void main(String[] args) {
+
+        Film film = new Film("The Godfather", 1972, true, 180, 9.5, "Francis Ford Coppola");
+        Film film2 = new Film("Avatar", 2009, true, 200, 8.5, "James Cameron");
+        Film film3 = new Film("Rambo", 1982, true, 200, 10, "Sylvester Stallone");
+        Series lost = new Series("Lost", 2004);
+        ArrayList<Title> whatchedList = new ArrayList<>();
+        whatchedList.add(film);
+        film.setRate(9.0);
+        whatchedList.add(film2);
+        film2.setRate(6);
+        whatchedList.add(film3);
+        film3.setRate(10);
+        whatchedList.add(lost);
+        // whatchedList.forEach(System.out::println);
+        for (Title item : whatchedList) {
+          System.out.println(item.getName());
+
+          if (item instanceof Film filmItem && filmItem.getClassification() > 3) {
+            System.out.println("Classification: " + filmItem.getClassification());
+          }
+        }
+    }
+
+}
